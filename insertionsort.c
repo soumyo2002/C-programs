1e3cb520a006d525736382549d55919337ed8426
Soumyo Chakravorty <92665254+soumyo2002@users.noreply.github.com>
2022-09-18 09:25:11 +0530

    Add files via upload

---
+++ insertionsort.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void insertion_sort(int a[],int n)
+{
+    int j=1,k=0;
+    for(int i=0;i<=(n-2);i++)
+    {
+        while(k>=0)
+        {
+            
+        }
+    }
+}
+
+int main()
+{
+    int arr[]={5,4,3,2,1};
+    int n=5;
+    insertion_sort(arr,n);
+}
