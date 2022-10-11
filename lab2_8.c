591177a55774ccb0e3b3fc5bb2cdef8e1c04150b
Soumyo Chakravorty <92665254+soumyo2002@users.noreply.github.com>
2022-10-11 10:49:09 +0530

    Add files via upload

---
+++ lab2_8.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a,max=0,sum=0;
+    float avg;
+    printf("Enter the array size \n");
+    scanf("%d",&a);
+    int a1[a];
+    printf("Enter the array elements: \n");
+    for(int i=0;i<a;i++)
+    {
+        scanf("%d",&a1[i]);
+    }
+    int min=a1[0];
+    for(int i=0;i<a;i++)
+    {
+        if(a1[i]>max)
+        {
+            max=a1[i];
+        }
+        if(a1[i]<min)
+        {
+            min=a1[i];
+        }
+        sum=sum+a1[i];
+    }
+    avg=sum/a;
+    printf(" Maximum element=%d , Minimum element=%d , average=%f ",max,min,avg);
+    return 0;
+}
