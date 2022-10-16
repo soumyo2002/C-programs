2f66eb027ea5c0314357387b35f0b3846c191c60
Soumyo Chakravorty <92665254+soumyo2002@users.noreply.github.com>
2022-10-16 09:53:03 +0530

    Add files via upload

---
+++ lab3_1.cpp
@@ -0,0 +1,13 @@
+/*Using class and object
+Q1. Wap to display numbers from 10 to 1 using for loop.
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=10;i>=1;i--)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
