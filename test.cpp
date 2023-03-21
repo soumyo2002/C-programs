44e1d5e62b9c724c2c357934cce317a5e579134b
Soumyo Chakravorty <92665254+soumyo2002@users.noreply.github.com>
2023-03-21 16:52:55 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<sstream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    const char* s="45";
+    int a= atoi(s);
+    cout<<a;
+    return 0;
+}
